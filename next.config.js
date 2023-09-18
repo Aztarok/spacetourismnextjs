da584077edd17f3d83793fe959d54584e415e391
Aztarok <aztarok2994@gmail.com>
2023-09-18 14:46:21 -0400

    testing deploy 3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: "export",
-	images: {
-		domains: ["aztarok.github.io/spacetourismnextjs"],
-	},
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {};
 
 module.exports = nextConfig;
